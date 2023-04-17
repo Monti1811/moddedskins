712a15d6d57af2687cc72b611b1a0e77d59eb2f9
Timon Doerzapf <s9tidoer@stud.uni-saarland.de>
2023-04-17 20:05:42 +0200

    Add ability to call a post_fn for gifts and add session specific gifts

--- scripts/tools/themoddedinventory.lua
+++ scripts/tools/themoddedinventory.lua
@@ -1,9 +1,13 @@
 local skin_queue = {}
 local unlocked_skins = {}
+--Add a new table for skins that are only unlocked in this world
+local unlocked_skins_sessions = {}
 
 local TheModdedInventory = Class(function(self)
     self.conditions = {}
 	self.isopeningmodgift = false
+    self.session_id = TheNet:GetSessionIdentifier()
+    unlocked_skins_sessions[self.session_id] = {}
 
     self:Load()
 end)
@@ -23,18 +27,37 @@ function TheModdedInventory:GetNextInQueue()
 end
 
 function TheModdedInventory:CheckOwnership(skin_id)
-    return unlocked_skins[skin_id] ~= nil
+    return unlocked_skins[skin_id] ~= nil or (unlocked_skins_sessions[self.session_id] ~= nil and unlocked_skins_sessions[self.session_id][skin_id] ~= nil)
+end
+
+local function MakeClientOwned(self, skin_id)
+    local condition = self.conditions[skin_id]
+    if condition.session_specific then
+        unlocked_skins_sessions[self.session_id][skin_id] = true
+    else
+        unlocked_skins[skin_id] = true
+    end
+    SendModRPCToServer(GetModRPC("ModdedSkins", "SetClientOwned"), skin_id, true)
+    -- Add a post_fn which gives the ability to unlock other gifts at the same time
+    -- or for other things
+    if condition.post_fn ~= nil then
+        condition.post_fn()
+    end
 end
 
 function TheModdedInventory:UnlockSkin(skin_id)
     local condition = self.conditions[skin_id]
     if condition then
         if condition.no_gift then
-            unlocked_skins[skin_id] = true
-			SendModRPCToServer(GetModRPC("ModdedSkins", "SetClientOwned"), skin_id, true)
-        elseif not unlocked_skins[skin_id] then --Let's not take a step backwards if the gift's been opened
-            skin_queue[skin_id] = true
-			SendModRPCToServer(GetModRPC("ModdedSkins", "RefreshModGiftCount"))
+            MakeClientOwned(self, skin_id)
+        else
+            -- Should only be added if a world specific skin hasn't been unlocked
+            local should_add_to_queue = condition.session_specific and not
+                (unlocked_skins_sessions[self.session_id] ~= nil and unlocked_skins_sessions[self.session_id][skin_id]) or not unlocked_skins[skin_id]
+            if should_add_to_queue then --Let's not take a step backwards if the gift's been opened
+                skin_queue[skin_id] = true
+                SendModRPCToServer(GetModRPC("ModdedSkins", "RefreshModGiftCount"))
+            end
         end
     end
 	self:Save()
@@ -43,8 +66,8 @@ end
 function TheModdedInventory:OpenGift(skin_id)
     if skin_queue[skin_id] then
         skin_queue[skin_id] = nil
-        unlocked_skins[skin_id] = true
-		SendModRPCToServer(GetModRPC("ModdedSkins", "SetClientOwned"), skin_id, true)
+
+        MakeClientOwned(self, skin_id)
 
 		self.isopeningmodgift = false
 		self:Save()
@@ -54,22 +77,34 @@ end
 
 function TheModdedInventory:LockSkin(skin_id) --TODO only send rpc is skin is in skin queue, no need to send the rpc if its only in the unlocked_skins
     skin_queue[skin_id] = nil
-    unlocked_skins[skin_id] = nil
+    local condition = self.conditions[skin_id]
+    if condition.session_specific then
+        unlocked_skins_sessions[self.session_id][skin_id] = nil
+    else
+        unlocked_skins[skin_id] = nil
+    end
 	SendModRPCToServer(GetModRPC("ModdedSkins", "SetClientOwned"), skin_id, false)
 	SendModRPCToServer(GetModRPC("ModdedSkins", "RefreshModGiftCount"))
 	self:Save()
 end
 
 function TheModdedInventory:SetClientOwnedSkins()
-	for skin_id, unlocked in pairs(unlocked_skins) do
-		if CLIENT_MOD_RPC["ModdedSkins"] then
-			SendModRPCToServer(GetModRPC("ModdedSkins", "SetClientOwned"), skin_id, unlocked)
-		end
-	end
+    for skin_id, unlocked in pairs(unlocked_skins) do
+        -- Check if skins have the session_specific condition and only mark them as owned
+        -- if they are owned in unlocked_skins_sessions[self.session_id]
+        if CLIENT_MOD_RPC["ModdedSkins"] and not (self.conditions[skin_id] and self.conditions[skin_id].session_specific) then
+            SendModRPCToServer(GetModRPC("ModdedSkins", "SetClientOwned"), skin_id, unlocked)
+        end
+    end
+    for skin_id, unlocked in pairs(unlocked_skins_sessions[self.session_id]) do
+        if CLIENT_MOD_RPC["ModdedSkins"] then
+            SendModRPCToServer(GetModRPC("ModdedSkins", "SetClientOwned"), skin_id, unlocked)
+        end
+    end
 end
 
 function TheModdedInventory:Save()
-    local str = json.encode({skin_queue = skin_queue, unlocked_skins = unlocked_skins})
+    local str = json.encode({skin_queue = skin_queue, unlocked_skins = unlocked_skins, unlocked_skins_sessions = unlocked_skins_sessions})
     TheSim:SetPersistentString("moddedskins", str, false)
 end
 
@@ -80,8 +115,10 @@ function TheModdedInventory:Load()
             if status and invdata then
                 skin_queue = invdata.skin_queue or {}
                 unlocked_skins = invdata.unlocked_skins or {}
+                unlocked_skins_sessions = invdata.unlocked_skins_sessions or {}
+                unlocked_skins_sessions[self.session_id] = unlocked_skins_sessions[self.session_id] or {}
             else
-                print("Faild to load TheModdedInventory!", status, invdata)
+                print("Failed to load TheModdedInventory!", status, invdata)
             end
         end
     end)
